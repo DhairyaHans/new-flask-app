b2db974b879a1fa49d43e1c3b91c29cf9e67ec3f
Dhairya Hans <51516879+DhairyaHans@users.noreply.github.com>
2023-11-08 18:41:25 +0530

    Update __init__.py

--- app_files/__init__.py
+++ app_files/__init__.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '150156a681c95a8ef057a4a5'
 
 # Connecting with database
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/employee'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/employee'
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://dhairya:password@my_flask_assignment_db:3306/codingthunder"
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://python:password@python_flask_db:3306/employee"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://python:password@0.0.0.0/employee"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://python:password@0.0.0.0/employee"
 
 db = SQLAlchemy(app)
 
